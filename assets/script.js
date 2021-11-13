a18f44fee0dca25a0f472271fe9bb363b37c831c
jillsuarez <jillsuarez@live.com>
2021-11-13 12:54:47 -0500

    hide cocktail image

--- assets/script.js
+++ assets/script.js
@@ -9,12 +9,11 @@ function getRandomDrink () {
         console.log(data);
         var drinkId = data.drinks[0].idDrink
         console.log(drinkId);
-       // getDrinkDetails(drinkId)
-       var img = document.createElement("img");
+       /*var img = document.createElement("img");
        img.style.css = "width:500px; height:auto";
        img.setAttribute("src",data.drinks[0].strDrinkThumb)
        console.log(img)
-       mainDiv.appendChild(img)
+       mainDiv.appendChild(img)*/
     })
 }
 
