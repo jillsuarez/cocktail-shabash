e7347c89907082a299d95b24b1a4f5db0c7a6125
jillsuarez <jillsuarez@live.com>
2021-11-13 11:47:08 -0500

    initial movie api link

--- assets/script.js
+++ assets/script.js
@@ -18,4 +18,37 @@ function getRandomDrink () {
     })
 }
 
+function getLatestMovie () {
+    fetch("https://api.themoviedb.org/3/movie/897693?api_key=b61f9e5cd6af472f99fe271ee07c0fcb&language=en-US").then(function(response){
+        return response.json()
+    }).then(data => {
+        console.log(data);
+       /* var drinkId = data.drinks[0].idDrink
+        console.log(drinkId);
+       // getDrinkDetails(drinkId)
+       var img = document.createElement("img");
+       img.style.css = "width:500px; height:auto";
+       img.setAttribute("src",data.drinks[0].strDrinkThumb)
+       console.log(img)
+       mainDiv.appendChild(img)*/
+    })
+}
+
+/*function getRandomMovie () {
+    fetch("https://api.themoviedb.org/3/movie/897693?api_key=b61f9e5cd6af472f99fe271ee07c0fcb&language=en-US").then(function(response){
+        return response.json()
+    }).then(data => {
+        console.log(data);
+        var drinkId = data.drinks[0].idDrink
+        console.log(drinkId);
+       // getDrinkDetails(drinkId)
+       var img = document.createElement("img");
+       img.style.css = "width:500px; height:auto";
+       img.setAttribute("src",data.drinks[0].strDrinkThumb)
+       console.log(img)
+       mainDiv.appendChild(img)
+    })*/
+
+
+getLatestMovie();
 getRandomDrink();
