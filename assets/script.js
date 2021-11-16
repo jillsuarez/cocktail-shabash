1a16e1b5406eb899f385f082df1f7265223d7743
KSWES <89271040+KSWES@users.noreply.github.com>
2021-11-16 16:33:55 -0500

    Add files via upload
    New changes to comment section for comparison

--- assets/script.js
+++ assets/script.js
@@ -1,15 +1,31 @@
-var mainDiv = document.getElementById("main")
+var mainDiv = document.getElementById("cocktail-i");
+var felid = document.querySelector('textarea'); // comment Section textarea
+var backUp = felid.getAttribute('placeholder'); // comment Section
+var commentBtn = document.querySelector('.comment-btn'); // comment Section
+var clear = document.getElementById('clear');  ///clear textarea 
+var cocktailNameDiv = document.getElementById("cockatil-d");
+var movieNameDiv = document.getElementById("movie-d");
+
+felid.onfocus = function() {     /// textarea 
+    this.setAttribute('placeholder', '');
+    this.style.borderColor = "hotpink";
+    commentBtn.style.display = "block"
+};
+
+felid.onblur = function(){     
+    this.setAttribute('placeholder', backUp);
+    this.style.borderColor = "hotpink"
+};
+
+clear.onclick = function() {
+    commentBtn.style.display = "none";
+    felid.value = "";
+}
+
+
+
 
 mainDiv.setAttribute("class", "container");
-
-document.getElementById("myshabash").addEventListener("click", getRandomDrink)
-
-/*Need to test button to know what comes up
-Make a div in the cocktail container
-(In the html)
-Get the description data from the API
-Use document.getElementByID(“name”).innerHTML = description
-*/
 
 function getRandomDrink () {
     fetch("https://www.thecocktaildb.com/api/json/v1/1/random.php").then(function(response){
@@ -18,65 +34,69 @@ function getRandomDrink () {
         console.log(data);
         var drinkId = data.drinks[0].idDrink
         console.log(drinkId);
-       /*var img = document.createElement("img");
-       img.style.css = "width:500px; height:auto";
-       img.setAttribute("src",data.drinks[0].strDrinkThumb)
-       console.log(img)
-       mainDiv.appendChild(img)*/
-    })
-}
-
-document.getElementById("myshabash").addEventListener("click", getLatestMovie)
-
-function getLatestMovie () {
-    fetch("https://api.themoviedb.org/3/movie/897693?api_key=b61f9e5cd6af472f99fe271ee07c0fcb&language=en-US").then(function(response){
-        return response.json()
-    }).then(data => {
-        console.log(data);
-       /* var drinkId = data.drinks[0].idDrink
-        console.log(drinkId);
-       // getDrinkDetails(drinkId)
        var img = document.createElement("img");
-       img.style.css = "width:500px; height:auto";
-       img.setAttribute("src",data.drinks[0].strDrinkThumb)
-       console.log(img)
-       mainDiv.appendChild(img)*/
-    })
-}
-
-/*function getRandomMovie () {
-    fetch("https://api.themoviedb.org/3/movie/897693?api_key=b61f9e5cd6af472f99fe271ee07c0fcb&language=en-US").then(function(response){
-        return response.json()
-    }).then(data => {
-        console.log(data);
-        var drinkId = data.drinks[0].idDrink
-        console.log(drinkId);
-       // getDrinkDetails(drinkId)
-       var img = document.createElement("img");
-       img.style.css = "width:500px; height:auto";
        img.setAttribute("src",data.drinks[0].strDrinkThumb)
        console.log(img)
        mainDiv.appendChild(img)
-    })*/
+       var cocktailName = document.createElement("div")
+       cocktailName.setAttribute("p", data.drinks[0].strDrink)
+       console.log(cocktailName)
+       cocktailNameDiv.appendChild(cocktailName)
+    
+       })
+    }
 
+function getRandomMovie () {
+    fetch("https://api.themoviedb.org/3/movie/popular?api_key=b61f9e5cd6af472f99fe271ee07c0fcb&language=en-US&page=1").then(function(response){
+        return response.json()
+    }).then(data => {
+        console.log(data);
+        var findRandomMovie = Math.floor(Math.random() * 20)
+        var popularMovies = data.results[findRandomMovie]
+        console.log(popularMovies)
+        var randomMovie = popularMovies.original_title
+        console.log(randomMovie)
+        var movieName = document.createElement("div")
+        movieName.setAttribute("p", randomMovie)
+        movieNameDiv.appendChild(movieName)
+    }) 
+}
 
-getLatestMovie();
+getRandomMovie();
 getRandomDrink();
 
 function storageAct() {
-    var usercomment=[document.getElementById("inserttextboxname").value[0],"user57821"]
+    var usercomment=[document.getElementById("commenttext").value[0],"user57821"]
     window.localStorage.setItem('user', JSON.stringify(usercomment));
 }
 /*Storing the comment*/
+
+var post= document.getElementById("comment-btn");
+
+var commentBoxValue= document.getElementById("commenttext").value;
+ 
+var li = document.createElement("li");
+var text = document.createTextNode(commentBoxValue);
+li.appendChild(text);
+document.getElementById("unordered").appendChild(li); 
+ 
+
 
 function displayComment() {
     const list=[]
     for (var i = 0; i < localStorage.length; i++){
         list.push(localStorage.getItem(localStorage.key(i)))
     }
-    document.getElementById("insertlistelementname").innerHTML=list
-    document.getElementById("insertlistelementname").style.display ='block'
+    document.getElementById("unordered").innerHTML=list
+    document.getElementById("unordered").style.display ='block'
 
 }
 /*Retrieving the comment. Field where comment will pop up will be hidden at first, then appear once comment is submitted*/
 /*An unordered list element needs to be created in html, when ul is defined, say list will be hidden*/
+
+
+post.addEventListener("click", storageAct)
+post.addEventListener("click", displayComment)
+
+document.getElementById("myshabash").addEventListener("click", getRandomDrink)
+document.getElementById("myshabash").addEventListener("click", getRandomMovie)
