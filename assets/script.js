90f15667e60016ed3f6d2ecc3bc64236b36eb7ff
KSWES <89271040+KSWES@users.noreply.github.com>
2021-11-17 06:44:54 -0500

    Add files via upload

--- assets/script.js
+++ assets/script.js
@@ -3,8 +3,9 @@ var felid = document.querySelector('textarea'); // comment Section textarea
 var backUp = felid.getAttribute('placeholder'); // comment Section
 var commentBtn = document.querySelector('.comment-btn'); // comment Section
 var clear = document.getElementById('clear');  ///clear textarea 
-var cocktailNameDiv = document.getElementById("cockatil-d");
+var cocktailNameDiv = document.getElementById("cocktail-d");
 var movieNameDiv = document.getElementById("movie-d");
+var movieImageDiv = document.getElementById("movie-lives");
 
 felid.onfocus = function() {     /// textarea 
     this.setAttribute('placeholder', '');
@@ -17,7 +18,7 @@ felid.onblur = function(){
     this.style.borderColor = "hotpink"
 };
 
-clear.onclick = function() {
+clear.onclick = function() {     //// comment text area clear 
     commentBtn.style.display = "none";
     felid.value = "";
 }
@@ -38,8 +39,8 @@ function getRandomDrink () {
        img.setAttribute("src",data.drinks[0].strDrinkThumb)
        console.log(img)
        mainDiv.appendChild(img)
-       var cocktailName = document.createElement("div")
-       cocktailName.setAttribute("p", data.drinks[0].strDrink)
+       var cocktailName = document.createElement("h1")
+       cocktailName.innerHTML = data.drinks[0].strDrink
        console.log(cocktailName)
        cocktailNameDiv.appendChild(cocktailName)
     
@@ -56,47 +57,80 @@ function getRandomMovie () {
         console.log(popularMovies)
         var randomMovie = popularMovies.original_title
         console.log(randomMovie)
-        var movieName = document.createElement("div")
-        movieName.setAttribute("p", randomMovie)
+        var movieId = popularMovies.id
+        console.log(movieId, "this is our first first api")
+        var movieName = document.createElement("h1")
+        movieName.innerHTML = randomMovie
         movieNameDiv.appendChild(movieName)
+        var posterPath = popularMovies.poster_path
+        console.log(posterPath)
+        configuration(posterPath);
+
     }) 
+}
+
+
+function configuration(posterPath) {
+    fetch("https://api.themoviedb.org/3/configuration?api_key=b61f9e5cd6af472f99fe271ee07c0fcb").then(function(response){
+        return response.json()
+    }).then(data => {
+        console.log(data, "this is the configuration api");
+        var baseUrl = data.images.base_url
+        console.log(baseUrl)
+        var posterSize = data.images.poster_sizes[2]
+        console.log(posterSize)
+        var movieImage = document.createElement("img");
+        movieImage.setAttribute("src",  baseUrl + "/" + posterSize + posterPath)
+        movieImageDiv.appendChild(movieImage)
+        console.log(movieImage)
+
+    })
 }
 
 getRandomMovie();
 getRandomDrink();
 
-function storageAct() {
-    var usercomment=[document.getElementById("commenttext").value[0],"user57821"]
-    window.localStorage.setItem('user', JSON.stringify(usercomment));
+
+const form = document.querySelector('form');
+const ul = document.querySelector('ul');
+const button = document.querySelector('button');
+const input = document.getElementById('comment');
+let itemsArray = localStorage.getItem('comment') ? JSON.parse(localStorage.getItem('comment')) : [];
+
+localStorage.setItem('comment', JSON.stringify(itemsArray));
+const data = JSON.parse(localStorage.getItem('comment'));
+
+const liMaker = (text) => {
+  const li = document.createElement('li');
+  li.textContent = text;
+  ul.appendChild(li);
 }
-/*Storing the comment*/
 
-var post= document.getElementById("comment-btn");
+form.addEventListener('submit', function (e) {
+  e.preventDefault();
 
-var commentBoxValue= document.getElementById("commenttext").value;
- 
-var li = document.createElement("li");
-var text = document.createTextNode(commentBoxValue);
-li.appendChild(text);
-document.getElementById("unordered").appendChild(li); 
- 
+  itemsArray.push(input.value);
+  localStorage.setItem('comment', JSON.stringify(itemsArray));
+  liMaker(input.value);
+  input.value = "";
+});
 
+data.forEach(item => {
+  liMaker(item);
+});
 
-function displayComment() {
-    const list=[]
-    for (var i = 0; i < localStorage.length; i++){
-        list.push(localStorage.getItem(localStorage.key(i)))
-    }
-    document.getElementById("unordered").innerHTML=list
-    document.getElementById("unordered").style.display ='block'
-
-}
-/*Retrieving the comment. Field where comment will pop up will be hidden at first, then appear once comment is submitted*/
-/*An unordered list element needs to be created in html, when ul is defined, say list will be hidden*/
+button.addEventListener('click', function () {
+  localStorage.clear();
+  while (ul.firstChild) {
+    ul.removeChild(ul.firstChild);
+  }
+  itemsArray = [];
+});
 
 
-post.addEventListener("click", storageAct)
-post.addEventListener("click", displayComment)
+liMaker();
 
-document.getElementById("myshabash").addEventListener("click", getRandomDrink)
-document.getElementById("myshabash").addEventListener("click", getRandomMovie)
+/*https://www.taniarascia.com/how-to-use-local-storage-with-javascript/ */
+
+document.getElementById('myshabash').addEventListener('click', getRandomDrink)
+document.getElementById('myshabash').addEventListener('click', getRandomMovie)
